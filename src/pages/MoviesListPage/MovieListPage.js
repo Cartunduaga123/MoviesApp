888297da32d51821ee84aaf09925555500a70497
Cartunduaga123 <52115389+Cartunduaga123@users.noreply.github.com>
2024-05-11 23:19:57 -0500

    despliegue ajuste

--- src/pages/MoviesListPage/MovieListPage.js
+++ src/pages/MoviesListPage/MovieListPage.js
@@ -16,7 +16,14 @@ function MovieListPage() {
   }, []);
 
   return (
-    <h1>Hola</h1>
+    <div className="m-12">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {movies.map(movie => (
+        //   <MovieCard key={movie.id} movie={movie} genres={genres}/>
+        <p key={movie.id}>{movie.title}</p>
+        ))}
+      </div>
+    </div>
   );
 }
 
