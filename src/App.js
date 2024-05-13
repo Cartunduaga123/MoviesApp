e633318c7c52c93da5ae6560fb68d17fc9a3856a
Cartunduaga123 <52115389+Cartunduaga123@users.noreply.github.com>
2024-05-13 12:26:32 -0500

    efectos header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import HeaderComponent from './components/Header/Header';
 import MovieListPage from './pages/MoviesListPage/MovieListPage';
 import MovieDetailPage from './pages/MoviesDetail/MoviesDetail';
@@ -8,17 +8,24 @@ function App() {
   return (
     <Router>
       <div className='App'>
+        <div className="background-color"></div>
         <HeaderComponent />
-        <Routes>
-          <Route path="/" element={<MovieListPage />} />
-          <Route path="/movie/:id" element={<MovieDetailPage />} />
-          {/* <Route path="/peliculas" component={Peliculas} />
-          <Route path="/series" component={Series} />
-          <Route path="/acerca-de" component={AcercaDe} />
-          <Route path="/contacto" component={Contacto} /> */}
-        </Routes>
+        <Content />
       </div>
     </Router>
+  );
+}
+
+function Content() {
+  const location = useLocation();
+
+  return (
+    <div className="content" style={{ paddingTop: '100px' }}> {/* Asegura que el contenido no quede oculto detrás del encabezado */}
+      <Routes location={location}>
+        <Route path="/" element={<MovieListPage />} />
+        <Route path="/movie/:id" element={<MovieDetailPage />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,34 @@
-import React, { useState } from 'react';
+// HeaderComponent.js
+
+import React, { useState, useEffect } from 'react';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 import './Header.css';
 
 function HeaderComponent() {
     const [nav, setNav] = useState(false);
+    const [scrolled, setScrolled] = useState(false);
+    const [hidden, setHidden] = useState(false);
+    const [lastScrollTop, setLastScrollTop] = useState(0);
 
-    // Toggle function to handle the navbar's display
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            setHidden(currentScrollTop > lastScrollTop && currentScrollTop > 950);
+            setScrolled(currentScrollTop > 0);
+            setLastScrollTop(currentScrollTop <= 0 ? 0 : currentScrollTop);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [lastScrollTop]);
+
     const handleNav = () => {
         setNav(!nav);
     };
 
-    // Array containing navigation items
     const navItems = [
         { id: 1, text: 'Home' },
         { id: 2, text: 'Company' },
@@ -20,48 +38,50 @@ function HeaderComponent() {
     ];
 
     return (
-        <div className='bg-black flex justify-between items-center h-24 max-w-[100%] mx-auto px-4 text-white'>
-            {/* Logo */}
-            <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACTS</h1>
+        <div className={`HeaderComponent ${scrolled ? 'scrolled' : ''} ${hidden ? 'hidden' : 'visible'}`}>
+            <div className='bg-[#17202A ] flex justify-between items-center h-24 max-w-[100%] mx-auto px-4 text-white'>
+                {/* Logo */}
+                <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Cineflix</h1>
 
-            {/* Desktop Navigation */}
-            <ul className='hidden md:flex'>
-                {navItems.map(item => (
-                    <li
-                        key={item.id}
-                        className='p-4 hover:bg-[#00df9a] rounded-xl m-2 cursor-pointer duration-300 hover:text-black'
-                    >
-                        {item.text}
-                    </li>
-                ))}
-            </ul>
+                {/* Desktop Navigation */}
+                <ul className='hidden md:flex'>
+                    {navItems.map(item => (
+                        <li
+                            key={item.id}
+                            className='p-4 hover:bg-[#00df9a] rounded-xl m-2 cursor-pointer duration-300 hover:text-black'
+                        >
+                            {item.text}
+                        </li>
+                    ))}
+                </ul>
 
-            {/* Mobile Navigation Icon */}
-            <div onClick={handleNav} className='block md:hidden'>
-                {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+                {/* Mobile Navigation Icon */}
+                <div onClick={handleNav} className='block md:hidden'>
+                    {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+                </div>
+
+                {/* Mobile Navigation Menu */}
+                <ul
+                    className={
+                        nav
+                            ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
+                            : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
+                    }
+                >
+                    {/* Mobile Logo */}
+                    <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>Cineflix.</h1>
+
+                    {/* Mobile Navigation Items */}
+                    {navItems.map(item => (
+                        <li
+                            key={item.id}
+                            className='p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600'
+                        >
+                            {item.text}
+                        </li>
+                    ))}
+                </ul>
             </div>
-
-            {/* Mobile Navigation Menu */}
-            <ul
-                className={
-                    nav
-                        ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
-                        : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
-                }
-            >
-                {/* Mobile Logo */}
-                <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACTS.</h1>
-
-                {/* Mobile Navigation Items */}
-                {navItems.map(item => (
-                    <li
-                        key={item.id}
-                        className='p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600'
-                    >
-                        {item.text}
-                    </li>
-                ))}
-            </ul>
         </div>
     );
 }
